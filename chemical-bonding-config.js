03a466e27cd06a5c46015136062617af90647874
pinebang <pine.bang@gmail.com>
2026-07-12 09:16:02 +0800

    Connect chemical bonding practice to Google Sheets

--- chemical-bonding-config.js
+++ chemical-bonding-config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_APPS_SCRIPT_URL = "";
+export const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyRsrPEn5Hpojlgx1e2Wxdq-lFCfrSR-IxXbmTq5j93Fwm3srCJc5mJKcQkwj75bqU/exec";
